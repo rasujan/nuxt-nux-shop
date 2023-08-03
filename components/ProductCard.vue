aaf7b18ea53261892b3c931f499fff539120612d
rasujan-gurzu <sujan.maharjan@gurzu.com>
2023-08-03 21:28:03 +0545

    test: integration mixpanel

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,5 +1,8 @@
 <template>
-  <NuxtLink :to="`/products/${product.id}`">
+  <NuxtLink
+    :to="`/products/${product.id}`"
+    @click="() => trackProductClick(product)"
+  >
     <div class="card h-full shadow-md hover:shadow-xl cursor-pointer p-4">
       <img :src="product?.image" class="object-contain h-48 mx-auto mb-6" />
 
@@ -9,8 +12,19 @@
   </NuxtLink>
 </template>
 
-<script setup lang="ts">
+<script setup lang="js">
+import mixpanel from "mixpanel-browser";
 const { product } = defineProps(["product"]);
+
+const trackProductClick = (product) => {
+  console.log(
+    "🚀 ~ file: ProductCard.vue:20 ~ trackProductClick ~ product:",
+    product
+  );
+  mixpanel.track("Product Click", {
+    product: product,
+  });
+};
 </script>
 
 <style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <h1 @click="homeTrack">Home</h1>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam amet
       eveniet quo eligendi eius assumenda sed harum doloribus fugiat! Ad sint
@@ -9,7 +9,25 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="js">
+import mixpanel from "mixpanel-browser";
+
+mixpanel.init("13d149d2bcb9bb8d11500318e8e15850", {
+  debug: true,
+  track_pageview: true,
+  persistence: "localStorage",
+});
+
+mixpanel.identify(1);
+
+mixpanel.set_config({ persistence: "localStorage" });
+
+const homeTrack = () => {
+  mixpanel.track("Home", {
+    status: "ok",
+  });
+};
+</script>
 
 <style lang="scss" scoped>
 h1 {
